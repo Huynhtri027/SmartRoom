a1c81dd114df71a2515455b221d088f8fb2f463c
Ayush Kumar <ayushkr19@gmail.com>
2015-12-02 15:58:34 +0530

    Add refresh button to Projects list

--- app/src/main/java/ayushkumar/smartroomsop/LoadActivity.java
+++ app/src/main/java/ayushkumar/smartroomsop/LoadActivity.java
@@ -9,6 +9,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -187,6 +189,34 @@ public class LoadActivity extends AppCompatActivity {
          */
         EventBus.getDefault().unregister(this);
         super.onStop();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_load, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_debug:
+                /*
+                 * Show Developer options
+                 */
+                Intent i = new Intent(this, MainActivity.class);
+                startActivity(i);
+                break;
+            case R.id.action_refresh:
+                /*
+                 * Refresh Projects list
+                 */
+                if(mRecyclerViewAdapter != null){
+                    mRecyclerViewAdapter.refreshData();
+                }
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     /**
